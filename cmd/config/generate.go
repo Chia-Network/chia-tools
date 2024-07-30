37da4915086c190a47b1967a3cacb73ddc6c6014
Chris Marslender <chrismarslender@gmail.com>
2024-07-30 16:26:03 -0500

    Fill config from env + write config file

--- cmd/config/generate.go
+++ cmd/config/generate.go
@@ -2,11 +2,12 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/chia-network/go-chia-libs/pkg/config"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 // generateCmd generates a new chia config
@@ -14,13 +15,23 @@ var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generate a new chia configuration file",
 	Run: func(cmd *cobra.Command, args []string) {
-		cfg := config.ChiaConfig{}
+		cfg, err := config.LoadDefaultConfig()
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		err = cfg.FillValuesFromEnvironment()
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
 		out, err := yaml.Marshal(cfg)
 		if err != nil {
 			fmt.Printf("Error marshalling config: %s\n", err.Error())
 			os.Exit(1)
 		}
-		fmt.Print(string(out))
+
+		os.WriteFile("config.yml", out, 0655)
 	},
 }
 
