1748cc4bd62cbeb7d5f5cd0400e9049cdab7fdc0
Chris Marslender <chrismarslender@gmail.com>
2024-07-30 22:25:11 -0500

    Add generate certs command

--- cmd/certs/generate.go
+++ cmd/certs/generate.go
@@ -1,9 +1,10 @@
 package certs
 
 import (
-	"fmt"
-
+	"github.com/chia-network/go-chia-libs/pkg/tls"
+	"github.com/chia-network/go-modules/pkg/slogs"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // generateCmd represents the generate command
@@ -11,10 +12,16 @@ var generateCmd = &cobra.Command{
 	Use:   "generate",
 	Short: "Generates a full set of certificates for chia-blockchain",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("generate called")
+		err := tls.GenerateAllCerts(viper.GetString("cert-output"))
+		if err != nil {
+			slogs.Logr.Fatal("error generating certificates", "error", err)
+		}
 	},
 }
 
 func init() {
+	generateCmd.PersistentFlags().StringP("output", "o", "certs", "Output directory for certs")
+	cobra.CheckErr(viper.BindPFlag("cert-output", generateCmd.PersistentFlags().Lookup("output")))
+
 	certsCmd.AddCommand(generateCmd)
 }
